c3aac50596629861dab57f5969cf58dd4066cefa
TimPixel <tim.buyalski@gmail>
2024-11-14 16:56:43 +0300

    initial commit

---
+++ src/main/java/me/timpixel/fivelifes/FiveLifes.java
@@ -0,0 +1,19 @@
+package me.timpixel.fivelifes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FiveLifes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        //1. Setup teams
+        //2. Setup life values
+        //  2.1 Saving life values
+        //  2.2
+
+
+        System.out.println("Five Lifes plugin loaded successfully");
+    }
+
+}
